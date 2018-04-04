4a5241c43c99ee35e87acf53f95582d4f7e219f8
osanai <osanai@cr-040.local>
2018-04-04 10:34:34 +0900

    Initial Commit

---
+++ CustomLabel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CustomLabel
+//
+//  Created by osanai on 2018/04/04.
+//  Copyright © 2018年 osanai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
