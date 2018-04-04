2c7979972e39d53b8c7e076cd7e943428d2c7296
osanai <osanai@CR-040.local>
2018-04-04 18:52:29 +0900

    create

--- CustomLabel/CustomLabel/CustomLabel.swift
+++ CustomLabel/CustomLabel/CustomLabel.swift
@@ -9,17 +9,38 @@
 import UIKit
 
 class CustomLabel: UILabel {
+
+    var linespace:CGFloat = 0
+    var hiragino:Bool = false
+    var lineNum:Int = 0
+    var fontSize:CGFloat = 12
     
     override var text: String? {
         didSet {
-            setup()
+            create()
         }
     }
+    
+    func setup(_ linespace:CGFloat?, hiragino:Bool?, lineNum:Int?, fontSize:CGFloat?) {
+        self.linespace = linespace ?? 0
+        self.hiragino = hiragino ?? false
+        self.lineNum = lineNum ?? 0
+        self.fontSize = fontSize ?? 12.0
+    }
 
-    func setup() {
+    func create() {
+        if self.hiragino {
+            self.font = UIFont(name: "HiraginoSans-W3", size: self.fontSize)
+        }
+        else {
+            self.font = UIFont(name: self.font.fontName, size: self.fontSize)
+        }
+        
+        self.numberOfLines = self.lineNum
+        
         let attributedString = NSMutableAttributedString(string: self.text ?? "")
         let paragraphStyle = NSMutableParagraphStyle()
-        paragraphStyle.lineSpacing = 10
+        paragraphStyle.lineSpacing = self.linespace
         attributedString.addAttribute(NSAttributedStringKey.paragraphStyle, value:paragraphStyle, range:NSMakeRange(0, attributedString.length))
         self.attributedText = attributedString;
     }
--- CustomLabel/ViewController.swift
+++ CustomLabel/ViewController.swift
@@ -12,11 +12,11 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         let label = CustomLabel.init(frame: CGRect(x: 100, y: 100, width: 100, height: 200))
+        label.setup(15, hiragino: true, lineNum: 0, fontSize: 14)
+        
         label.backgroundColor = UIColor.yellow
-        label.numberOfLines = 0
-        label.font = UIFont(name: "HiraKakuProN-W3", size: 14)
         
         label.text = "あいうえおかきくけこさしすせそたちつてとなにぬねの"
         
